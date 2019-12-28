d7baecc3d60019f803ce4145b9d70cfeb81dc4a7
leandrofahur <fahurleandro@gmail.com>
2019-12-28 07:20:35 -0300

    alias to cmake 3.16

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/src/Graphics.cpp" "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/src/Intersection.cpp" "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/src/Street.cpp" "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/src/TrafficLight.cpp" "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/src/TrafficObject.cpp" "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/src/TrafficSimulator-Final.cpp" "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/src/Vehicle.cpp" "/home/leandro/Documents/repository/udacity/cpp-program-a-concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
