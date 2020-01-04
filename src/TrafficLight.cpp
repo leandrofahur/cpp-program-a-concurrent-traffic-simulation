d1c9e92c4c81d979cb5cb15f094c966de1022979
leandrofahur <fahurleandro@gmail.com>
2020-01-03 21:11:16 -0300

    FP.2:Implement a cycleThroughPhases method

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <random>
+#include <chrono>
+#include <thread>
+#include <ctime>
+#include <future>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+ 
 template <typename T>
 T MessageQueue<T>::receive()
 {
@@ -19,7 +23,7 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
  
@@ -43,6 +47,7 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -52,4 +57,54 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+
+
+    // Reference for random numbers in C++11:
+    //https://stackoverflow.com/questions/686353/random-float-number-generation
+    std::random_device rd;
+
+    // Engines 
+    //
+    std::mt19937 e2(rd());
+    //std::knuth_b e2(rd());
+    //std::default_random_engine e2(rd()) ;
+
+    //
+    // Distribtuions
+    //
+    std::uniform_real_distribution<int> dist(4000, 6000);
+    //std::normal_distribution<> dist(2, 2);
+    //std::student_t_distribution<> dist(5);
+    //std::poisson_distribution<> dist(2);
+    //std::extreme_value_distribution<> dist(0,2);
+
+    // int randomCycleValue = dist(e2);
+
+    std::chrono::time_point<std::chrono::system_clock> timeNow;
+
+    timeNow = std::chrono::system_clock::now();
+    while(true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        
+        int elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>
+                             (std::chrono::system_clock::now()-timeNow).count();
+
+        if(elapsed_seconds >= dist(e2))
+        {
+            if(this->_currentPhase == TrafficLightPhase::red)
+            {
+                this->_currentPhase = TrafficLightPhase::green;
+            }
+            else
+            {
+                this->_currentPhase = TrafficLightPhase::red;
+            }
+
+            std::future<TrafficLightPhase> futVar = std::async(&MessageQueue<TrafficLightPhase>::send, std::move(_currentPhase));
+            futVar.wait();
+        }
+
+        timeNow = std::chrono::system_clock::now();
+    }
 }
